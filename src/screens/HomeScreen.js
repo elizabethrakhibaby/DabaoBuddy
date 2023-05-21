8479c5003e97cd4834d057336b133f30f46ce3d0
elizabethrakhibaby <rbelizabeth9@gmail.com>
2023-05-22 01:16:19 +0800

    Adding navigation features
    Signed-off-by: elizabethrakhibaby <rbelizabeth9@gmail.com>

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+
+const HomeScreen = function() {
+
+    return (
+        <View>
+        <Text>This is the home screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+
+const ProfileScreen = function() {
+
+    return (
+        <View>
+        <Text>This is the Profile Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default ProfileScreen;
