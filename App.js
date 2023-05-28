927090ab402cad450865c28e32139d1a8581056c
Elizabeth Rakhi Baby <133880422+elizabethrakhibaby@users.noreply.github.com>
2023-05-28 17:56:53 +0800

    Update App.js

--- App.js
+++ App.js
@@ -67,52 +67,7 @@ export default App;
 
 
 
-/** 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from "../DabaoBuddy/src/screens/HomeScreen";
-import ProfileScreen from "../DabaoBuddy/src/screens/ProfileScreen";
 
-
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-person' : 'ios-person-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          //options={{ tabBarBadge: 3 }}
-        />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
 
 
 */
