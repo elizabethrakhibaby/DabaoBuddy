3e6d9ab090a7ee4fcdea237ca433aff28f71b624
elizabethrakhibaby <rbelizabeth9@gmail.com>
2023-06-17 16:25:27 +0800

    Created finance page + Error message for wrong input of email type
    Signed-off-by: elizabethrakhibaby <rbelizabeth9@gmail.com>

--- src/screens/FinancesScreen.js
+++ src/screens/FinancesScreen.js
@@ -1,20 +1,296 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { auth, firestore } from "./firebase";
+import firebase from 'firebase/compat/app';
+import Svg, { G, Circle } from "react-native-svg";
+//import { background, backgroundColor, justifyContent } from "styled-system";
+
+const FinancesScreen = function () {
+  const [expenditure, setExpenditure] = useState(null);
+  const [earnings, setEarnings] = useState(null);
+
+  useEffect(() => {
+    getCurrentUserExpenditure();
+  }, []);
+
+  const getCurrentUserExpenditure = () => {
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+      const uid = user.uid;
+      retrieveUserExpenditure(uid);
+    } else {
+      console.log('No user is currently logged in.');
+    }
+  };
+
+  const retrieveUserExpenditure = async (uid) => {
+    try {
+      const userDoc = await firestore.collection('users').doc(uid).get();
+
+      if (userDoc.exists) {
+        const expenditureData = userDoc.data().expenditure;
+        setExpenditure(expenditureData);
+      } else {
+        console.log('User document does not exist.');
+      }
+    } catch (error) {
+      console.error('Error retrieving expenditure data:', error);
+    }
+  };
+  //
+  useEffect(() => {
+    getCurrentUserEarnings();
+  }, []);
+
+  const getCurrentUserEarnings = () => {
+    const user = firebase.auth().currentUser;
+
+    if (user) {
+      const uid = user.uid;
+      retrieveUserEarnings(uid);
+    } else {
+      console.log('No user is currently logged in.');
+    }
+  };
+
+  const retrieveUserEarnings = async (uid) => {
+    try {
+      const userDoc = await firestore.collection('users').doc(uid).get();
+
+      if (userDoc.exists) {
+        const earningsData = userDoc.data().earnings;
+        setEarnings(earningsData);
+      } else {
+        console.log('User document does not exist.');
+      }
+    } catch (error) {
+      console.error('Error retrieving earnings data:', error);
+    }
+  };
+
+  /**
+   * Measurement for donut pie chart
+   */
+  const radius = 70;
+  const circleCircumference = 2 * Math.PI * radius;
+  const total = earnings + expenditure;
+  const net = earnings - expenditure;
+
+  const earningsPercentage = (earnings / total) * 100;
+  const expenditurePercentage = (expenditure / total) * 100;
+
+  const earningsStrokeDashoffset =
+    circleCircumference - (circleCircumference * earningsPercentage) / 100;
+  const expenditureStrokeDashoffset =
+    circleCircumference - (circleCircumference * expenditurePercentage) / 100;
+
+  const earningsAngle = (earnings / total) * 360;
+  const expenditureAngle = (expenditure / total) * 360;
+
+  /* This code will be used when user's order is delivered if user in Dabao mode*/
+  const increaseExpenditure = async (valueToAdd) => {
+    try {
+      const user = firebase.auth().currentUser;
+
+      if (user) {
+        const uid = user.uid;
+        const userRef = firestore.collection('users').doc(uid);
+
+        // Get the current expenditure value
+        const userDoc = await userRef.get();
+        const currentExpenditure = userDoc.data().expenditure || 0;
+
+        // Calculate the new expenditure value
+        const updatedExpenditure = currentExpenditure + valueToAdd;
+
+        // Update the expenditure field in the user document
+        await userRef.update({ expenditure: updatedExpenditure });
+
+        // Update the state to reflect the new expenditure
+        setExpenditure(updatedExpenditure);
+
+        console.log('Expenditure increased successfully!');
+      } else {
+        console.log('No user is currently logged in.');
+      }
+    } catch (error) {
+      console.error('Error increasing expenditure:', error);
+    }
+  };
+
+  /* This code will be used when user's order is delivered if user in Buddy mode*/
+  const increaseEarnings = async (valueToAdd) => {
+    try {
+      const user = firebase.auth().currentUser;
+
+      if (user) {
+        const uid = user.uid;
+        const userRef = firestore.collection('users').doc(uid);
+
+        // Get the current earnings value
+        const userDoc = await userRef.get();
+        const currentEarnings = userDoc.data().earnings || 0;
+
+        // Calculate the new earnings value
+        const updatedEarnings = currentEarnings + valueToAdd;
+
+        // Update the expenditure field in the user document
+        await userRef.update({ earnings: updatedEarnings });
+
+        // Update the state to reflect the new expenditure
+        setEarnings(updatedEarnings);
+
+        console.log('Earnings increased successfully!');
+      } else {
+        console.log('No user is currently logged in.');
+      }
+    } catch (error) {
+      console.error('Error increasing earnings:', error);
+    }
+  };
+
+  return (
+    <View style={{ backgroundColor: "EAE5D9", flex: 1 }}>
+      <Text style={styles.pageTitle}>Keep track of your finances!</Text>
+
+      <Button
+        onPress={() => increaseEarnings(10)}
+        title="Increase earnings by 10"
+        color="#841584"
+      />
+
+      <Button
+        onPress={() => increaseExpenditure(44)}
+        title="Increase expenditure by 44"
+        color="#841584"
+      />
+
+      <View style={styles.graphWrapper}>
+        <Svg height="270" width="270" viewBox="0 0 180 180">
+          <G rotation={-90} originX="90" originY="90">
+            {total === 0 ? (
+              <Circle
+                cx="50%"
+                cy="50%"
+                r={radius}
+                stroke="#83918b"
+                fill="transparent"
+                strokeWidth="40"
+              />
+            ) : (
+              <>
+                <Circle
+                  cx="50%"
+                  cy="50%"
+                  r={radius}
+                  stroke="#0057B7"
+                  fill="transparent"
+                  strokeWidth="40"
+                  strokeDasharray={circleCircumference}
+                  strokeDashoffset={earningsStrokeDashoffset}
+                  rotation={0}
+                  originX="90"
+                  originY="90"
+                  strokeLinecap="round"
+                />
 
 
-const FinancesScreen = function() {
-
-    return (
-        <View>
-        <Text>This is the Finances Screen.</Text>
+                <Circle
+                  cx="50%"
+                  cy="50%"
+                  r={radius}
+                  stroke="#ffd700"
+                  fill="transparent"
+                  strokeWidth="40"
+                  strokeDasharray={circleCircumference}
+                  strokeDashoffset={expenditureStrokeDashoffset}
+                  rotation={earningsAngle}
+                  originX="90"
+                  originY="90"
+                  strokeLinecap="round"
+                />
+              </>
+            )
+            }
+          </G>
+        </Svg>
+        <Text style={styles.label}>$ {net}</Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={styles.box1}>
+          <Text style={styles.infoHeading}>Overall Earnings</Text>
+          <Text style={styles.infoValue}>${earnings}</Text>
         </View>
-    );
+
+        <View style={styles.box2}>
+          <Text style={styles.infoHeading}>Overall Expenditure</Text>
+          <Text style={styles.infoValue}>${expenditure}</Text>
+        </View>
+
+      </View>
+
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
   },
+  pageTitle: {
+    fontSize: 50,
+    alignSelf: "center",
+    fontFamily: "Times New Roman"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e8ebd5"
+  },
+  graphWrapper: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10
+  },
+  label: {
+    position: "absolute",
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 24,
+  },
+  box1: {
+    width: 170,
+    height: 150,
+    backgroundColor: "#8CB1F7",
+    borderRadius: 15,
+    margin: 20,
+    alignItems: 'center',
+  },
+  box2: {
+    width: 170,
+    height: 150,
+    backgroundColor: "#E8EC77",
+    borderRadius: 15,
+    marginTop: 20,
+    marginRight: 20,
+    alignItems: 'center',
+  },
+  infoHeading: {
+    top: 10,
+    fontWeight: 'bold'
+  },
+  infoValue: {
+    top: 35,
+    fontSize: 50
+  }
 });
 
 export default FinancesScreen;
+
+
+
+
+
+
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,49 +1,49 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { NativeBaseProvider, Button, Icon, Box, Image, AspectRatio } from 'native-base';
 import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { alignContent, flex, flexDirection, width } from 'styled-system';
-import { auth } from './firebase';
+import { auth, firestore } from './firebase';
 
 
 
 function Signup() {
-    const navigation = useNavigation();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
-    
-    // const handleRegistration = () => {
-    //   navigation.navigate("Login");
-    // };
-
-    const handleRegistration = () => {
-      if (password === confirmPassword) {
-        auth
-          .createUserWithEmailAndPassword(email, password)
-          .then(userCredential => {
-            const user = userCredential.user;
-            console.log('Signup successful');
-            // Store additional user details in Firestore
-            firestore.collection('users').doc(user.uid).set({
-              email: user.email
-            });
-            setError(''); // Reset the error message
-            navigation.navigate('Login');
-          })
-          .catch(error => {
-            setError(error.message); // Set the error message
-            console.log('Signup failed:', error.message);
+  const handleRegistration = () => {
+    if (password === confirmPassword && email.endsWith("@u.nus.edu")) {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+          const user = userCredential.user;
+          console.log('Signup successful');
+          // Store additional user details in Firestore
+          firestore.collection('users').doc(user.uid).set({
+            email: user.email,
+            earnings: 0,
+            expenditure: 0,
+            name: ""
           });
-      } else {
-        setError('Passwords do not match'); // Set the error message
-      }
-    };
-  
+          setError(''); // Reset the error message
+          navigation.navigate('Login');
+        })
+        .catch(error => {
+          setError(error.message); // Set the error message
+          console.log('Signup failed:', error.message);
+        });
+    } else if (!email.endsWith("@u.nus.edu")) {
+      setError('Please use your NUS Email'); // Set the error message
+    } else {
+      setError('Passwords do not match'); // Set the error message
+    }
+  };
+
 
   return (
     //First 2 lines
@@ -58,16 +58,16 @@ function Signup() {
 
       {/* Username or Email Input Field */}
       <View style={styles.buttonStyleX}>
-      <TextInput
-          placeholder = "Email"
+        <TextInput
+          placeholder="Email"
           value={email}
-          onChangeText={text=> setEmail(text)}
-          />
+          onChangeText={text => setEmail(text)}
+        />
       </View>
 
       {/* Password Input Field */}
       <View style={styles.buttonStyleX}>
-      <TextInput
+        <TextInput
           placeholder="Password"
           value={password}
           onChangeText={text => setPassword(text)}
@@ -76,7 +76,7 @@ function Signup() {
       </View>
 
       <View style={styles.buttonStyleX}>
-      <TextInput
+        <TextInput
           placeholder="Confirm Password"
           value={confirmPassword}
           onChangeText={text => setConfirmPassword(text)}
@@ -84,14 +84,14 @@ function Signup() {
         />
       </View>
 
-       {/* Signup Button */}
-       <View style={styles.buttonStyle}>
+      {/* Signup Button */}
+      <View style={styles.buttonStyle}>
         <Button style={styles.buttonDesign} onPress={handleRegistration}>
-      <Text> SIGN UP </Text>
-    </Button>
-    {/* Error Message */}
-    <Text style={styles.errorText}>{error}</Text>
-      {/* Rest of your code */}
+          <Text> SIGN UP </Text>
+        </Button>
+        {/* Error Message */}
+        <Text style={styles.errorText}>{error}</Text>
+        {/* Rest of your code */}
       </View>
 
     </View>
@@ -101,9 +101,9 @@ function Signup() {
 export default () => {
   return (
     <NativeBaseProvider>
-     
-        <Signup />
-      
+
+      <Signup />
+
     </NativeBaseProvider>
   )
 }
@@ -115,66 +115,66 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   LoginText: {
-    marginTop:100,
-    fontSize:30,
-    fontWeight:'bold',
+    marginTop: 100,
+    fontSize: 30,
+    fontWeight: 'bold',
   },
-  Middle:{
-    alignItems:'center',
-    justifyContent:'center',
+  Middle: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text2:{
-    flexDirection:'row',
-    justifyContent:'center',
-    paddingTop:5
+  text2: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 5
   },
-  signupText:{
-    fontWeight:'bold'
+  signupText: {
+    fontWeight: 'bold'
   },
-  emailField:{
-    marginTop:30,
-    marginLeft:15
+  emailField: {
+    marginTop: 30,
+    marginLeft: 15
   },
-  emailInput:{
-    marginTop:10,
-    marginRight:5
+  emailInput: {
+    marginTop: 10,
+    marginRight: 5
   },
-  buttonStyle:{
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15
+  buttonStyle: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15
   },
-  buttonStyleX:{
-    marginTop:12,
-    marginLeft:15,
-    marginRight:15,
+  buttonStyleX: {
+    marginTop: 12,
+    marginLeft: 15,
+    marginRight: 15,
     marginBottom: 10
   },
-  buttonDesign:{
-    backgroundColor:'#FDDB62'
+  buttonDesign: {
+    backgroundColor: '#FDDB62'
   },
-  lineStyle:{
-    flexDirection:'row',
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15,
-    alignItems:'center'
+  lineStyle: {
+    flexDirection: 'row',
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    alignItems: 'center'
   },
-  imageStyle:{
-    width:80,
-    height:80,
-    marginLeft:20,
+  imageStyle: {
+    width: 80,
+    height: 80,
+    marginLeft: 20,
   },
-  boxStyle:{
-    flexDirection:'row',
-    marginTop:30,
-    marginLeft:15,
-    marginRight:15,
-    justifyContent:'space-around'
+  boxStyle: {
+    flexDirection: 'row',
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    justifyContent: 'space-around'
   },
   errorText: {
     color: 'red',
   },
-  
+
 });
 
